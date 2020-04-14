d245042f048f12a60599033311ad1e74e90b9db3
khan tauhid <khantahid313@gmail.com>
2020-04-14 21:22:42 +0530

    Added inferencing script

--- detect.py
+++ detect.py
@@ -1,31 +1,30 @@
 import cv2
 
-import tensorflow
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 import numpy as np
 
-
-model = load_model('covid-19_161.h5')
-
 classes = ['Covid-19','normal','pneumonia']
 
-img = cv2.imread('pbact1.jpeg')
+#load model
+model = load_model('model/trained_model/covid-19_161.h5')
 
+#read image
+img = cv2.imread('model/samples/pbact1.jpeg')
 
+#resize image
 img = cv2.resize(img,(256,256))
 
+#Converting to float32 and normalizing 
 img = image.img_to_array(img)
-
 img = np.expand_dims(img,axis=0)
 img/=255
 
+#predict
 pred = model.predict(img)
-print(pred[0])
 result = classes[np.argmax(pred[0])]
-
 print(result)
-
+print("Confidence",np.max(pred[0])*100)
 
 del(model)
